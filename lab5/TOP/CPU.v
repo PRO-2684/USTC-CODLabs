3704a889f7920ddbb2f0c277c9ca6d1c4558cc98
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2023-05-20 13:13:15 +0800

    Wiring bug fix

--- lab5/TOP/CPU.v
+++ lab5/TOP/CPU.v
@@ -30,7 +30,6 @@ module CPU(
     // 4. 进一步完成冒险处理模块，以通过数据冒险测试 data_test.asm。
     // 5. 用 pipeline_test.asm 进行必做部分最终测试。
     // 6. 阅读附加部分文档并量力完成选做。
-    // *ALU、*RF、Control、Add(Or just add-mode ALU)、And(Or just and-mode ALU)、PCReg、Imm、Branch、Mux、...
     wire [31:0] inst_raw;
     wire [31:0] dm_dout;
     wire [31:0] pc_cur_if;
@@ -52,7 +51,6 @@ module CPU(
     wire [31:0] rf_rd1_raw_ex;
     wire [31:0] imm_ex;
     wire [31:0] pc_add4_ex;
-    wire [31:0] dm_din_ex;
     wire [31:0] alu_ans_ex;
     wire [31:0] pc_jalr_ex;
     wire [31:0] rf_rd0_ex;
@@ -61,7 +59,6 @@ module CPU(
     wire [31:0] alu_src2_ex;
     wire [31:0] rf_rd0_fd;
     wire [31:0] rf_rd1_fd;
-    wire [31:0] rd1_ex;
     wire [31:0] pc_cur_mem;
     wire [31:0] inst_mem;
     wire [31:0] rf_ra0_mem;
@@ -118,16 +115,16 @@ module CPU(
     wire [2:0] imm_type_ex;
     wire [2:0] imm_type_mem;
     wire [2:0] imm_type_wb;
+    wire [2:0] br_type_id;
+    wire [2:0] br_type_ex;
+    wire [2:0] br_type_mem;
+    // wire [2:0] br_type_wb; // FIXME: Not used?
     wire [1:0] rf_wd_sel_id;
-    wire [1:0] br_type_id;
     wire [1:0] rf_wd_sel_ex;
-    wire [1:0] br_type_ex;
     wire [1:0] pc_sel_ex;
     wire [1:0] rf_wd_sel_mem;
-    wire [1:0] br_type_mem;
     wire [1:0] pc_sel_mem;
     wire [1:0] rf_wd_sel_wb;
-    wire [1:0] br_type_wb;
     wire [1:0] pc_sel_wb;
 
     wire rf_we_wb;
@@ -142,7 +139,6 @@ module CPU(
     wire alu_src2_sel_id;
     wire jal_id;
     wire jalr_id;
-    wire mem_we_id;
     wire dm_we_id;
     wire flush_ex;
     wire stall_ex;
@@ -169,8 +165,8 @@ module CPU(
     wire dm_we_mem;
     wire rf_re0_wb;
     wire rf_re1_wb;
-    wire alu_src1_sel_wb;
-    wire alu_src2_sel_wb;
+    // wire alu_src1_sel_wb;
+    // wire alu_src2_sel_wb;
     wire jal_wb;
     wire jalr_wb;
     wire br_wb;
@@ -222,7 +218,7 @@ module CPU(
         .pc_jalr_in(32'h0),
         .jal_in(1'h0),
         .jalr_in(1'h0),
-        .br_type_in(2'h0),
+        .br_type_in(3'h0),
         .br_in(1'h0),
         .pc_sel_in(2'h0),
         .pc_next_in(32'h0),
@@ -239,7 +235,7 @@ module CPU(
     );
     RF rf(.we(rf_we_wb), .clk(clk), .ra0(rf_ra0_id), .ra1(rf_ra1_id), .wa(rf_wa_wb), .wd(rf_wd_wb), .ra_dbg(cpu_check_addr[4:0]), .rd0(rf_rd0_raw_id), .rd1(rf_rd1_raw_id), .rd_dbg(rf_rd_dbg_id));
     Immediate immediate(.imm_type(imm_type_id), .inst(inst_id), .imm(imm_id));
-    CTRL ctrl(.inst(inst_id), .rf_re0(rf_re0_id), .rf_re1(rf_re1_id), .rf_wd_sel(rf_wd_sel_id), .rf_we(rf_we_id), .imm_type(imm_type_id), .alu_src1_sel(alu_src1_sel_id), .alu_src2_sel(alu_src2_sel_id), .alu_func(alu_func_id), .jal(jal_id), .jalr(jalr_id), .br_type(br_type_id), .mem_we(mem_we_id));
+    CTRL ctrl(.inst(inst_id), .rf_re0(rf_re0_id), .rf_re1(rf_re1_id), .rf_wd_sel(rf_wd_sel_id), .rf_we(rf_we_id), .imm_type(imm_type_id), .alu_src1_sel(alu_src1_sel_id), .alu_src2_sel(alu_src2_sel_id), .alu_func(alu_func_id), .jal(jal_id), .jalr(jalr_id), .br_type(br_type_id), .mem_we(dm_we_id));
     SEG_REG seg_reg_id_ex (
         .clk(clk),
         .flush(flush_ex),
@@ -299,7 +295,6 @@ module CPU(
         .jal_out(jal_ex),
         .jalr_out(jalr_ex),
         .br_type_out(br_type_ex),
-        .dm_din_out(dm_din_ex),
         .dm_we_out(dm_we_ex)
     );
     AND and_(.lhs(32'hFFFFFFFE), .rhs(alu_ans_ex), .res(pc_jalr_ex));
@@ -346,7 +341,7 @@ module CPU(
         .pc_sel_in(pc_sel_ex),
         .pc_next_in(pc_next),
         .dm_addr_in(alu_ans_ex),
-        .dm_din_in(rd1_ex),
+        .dm_din_in(rf_rd1_ex),
         .dm_dout_in(32'h0),
         .dm_we_in(dm_we_ex),
         .pc_cur_out(pc_cur_mem),
@@ -438,8 +433,8 @@ module CPU(
         .rf_we_out(rf_we_wb),
         .imm_type_out(imm_type_wb),
         .imm_out(imm_wb),
-        .alu_src1_sel_out(alu_src1_sel_wb),
-        .alu_src2_sel_out(alu_src2_sel_wb),
+        // .alu_src1_sel_out(alu_src1_sel_wb),
+        // .alu_src2_sel_out(alu_src2_sel_wb),
         .alu_src1_out(alu_src1_wb),
         .alu_src2_out(alu_src2_wb),
         .alu_func_out(alu_func_wb),
@@ -450,7 +445,7 @@ module CPU(
         .pc_jalr_out(pc_jalr_wb),
         .jal_out(jal_wb),
         .jalr_out(jalr_wb),
-        .br_type_out(br_type_wb),
+        // .br_type_out(br_type_wb),
         .br_out(br_wb),
         .pc_sel_out(pc_sel_wb),
         .pc_next_out(pc_next_wb),
@@ -474,10 +469,10 @@ module CPU(
 
     Check_Data_SEL check_data_sel_if(.pc_cur(pc_cur_if), .instruction(inst_if), .rf_ra0(inst_if[19:15]), .rf_ra1(inst_if[24:20]), .rf_re0(1'h0), .rf_re1(1'h0), .rf_rd0_raw(32'h0), .rf_rd1_raw(32'h0), .rf_rd0(32'h0), .rf_rd1(32'h0), .rf_wa(inst_if[11:7]), .rf_wd_sel(2'h0), .rf_wd(32'h0), .rf_we(1'h0), .immediate(32'h0), .alu_src1(32'h0), .alu_src2(32'h0), .alu_func(4'h0), .alu_ans(32'h0), .pc_add4(pc_add4_if), .pc_br(32'h0), .pc_jal(32'h0), .pc_jalr(32'h0), .pc_sel(2'h0), .pc_next(32'h0), .dm_addr(32'h0), .dm_din(32'h0), .dm_dout(32'h0), .dm_we(1'h0), .check_addr(cpu_check_addr[4:0]), .check_data(check_data_if));
     Check_Data_SEL check_data_sel_id(.pc_cur(pc_cur_id), .instruction(inst_id), .rf_ra0(rf_ra0_id), .rf_ra1(rf_ra1_id), .rf_re0(rf_re0_id), .rf_re1(rf_re1_id), .rf_rd0_raw(rf_rd0_raw_id), .rf_rd1_raw(rf_rd1_raw_id), .rf_rd0(32'h0), .rf_rd1(32'h0), .rf_wa(rf_wa_id), .rf_wd_sel(rf_wd_sel_id), .rf_wd(32'h0), .rf_we(rf_we_id), .immediate(imm_id), .alu_src1(32'h0), .alu_src2(32'h0), .alu_func(alu_func_id), .alu_ans(32'h0), .pc_add4(pc_add4_id), .pc_br(32'h0), .pc_jal(32'h0), .pc_jalr(32'h0), .pc_sel(2'h0), .pc_next(32'h0), .dm_addr(32'h0), .dm_din(rd1_id), .dm_dout(32'h0), .dm_we(dm_we_id), .check_addr(cpu_check_addr[4:0]), .check_data(check_data_id));
-    Check_Data_SEL check_data_sel_ex(.pc_cur(pc_cur_ex), .instruction(inst_ex), .rf_ra0(rf_ra0_ex), .rf_ra1(rf_ra1_ex), .rf_re0(rf_re0_ex), .rf_re1(rf_re1_ex), .rf_rd0_raw(rf_rd0_raw_ex), .rf_rd1_raw(rf_rd1_raw_ex), .rf_rd0(rf_rd0_ex), .rf_rd1(rf_rd1_ex), .rf_wa(rf_wa_ex), .rf_wd_sel(rf_wd_sel_ex), .rf_wd(32'h0), .rf_we(rf_we_ex), .immediate(imm_ex), .alu_src1(alu_src1_ex), .alu_src2(alu_src2_ex), .alu_func(alu_func_ex), .alu_ans(alu_ans_ex), .pc_add4(pc_add4_ex), .pc_br(alu_ans_ex), .pc_jal(alu_ans_ex), .pc_jalr(pc_jalr_ex), .pc_sel(pc_sel_ex), .pc_next(pc_next), .dm_addr(alu_ans_ex), .dm_din(dm_din_ex), .dm_dout(32'h0), .dm_we(dm_we_ex), .check_addr(cpu_check_addr[4:0]), .check_data(check_data_ex));
+    Check_Data_SEL check_data_sel_ex(.pc_cur(pc_cur_ex), .instruction(inst_ex), .rf_ra0(rf_ra0_ex), .rf_ra1(rf_ra1_ex), .rf_re0(rf_re0_ex), .rf_re1(rf_re1_ex), .rf_rd0_raw(rf_rd0_raw_ex), .rf_rd1_raw(rf_rd1_raw_ex), .rf_rd0(rf_rd0_ex), .rf_rd1(rf_rd1_ex), .rf_wa(rf_wa_ex), .rf_wd_sel(rf_wd_sel_ex), .rf_wd(32'h0), .rf_we(rf_we_ex), .immediate(imm_ex), .alu_src1(alu_src1_ex), .alu_src2(alu_src2_ex), .alu_func(alu_func_ex), .alu_ans(alu_ans_ex), .pc_add4(pc_add4_ex), .pc_br(alu_ans_ex), .pc_jal(alu_ans_ex), .pc_jalr(pc_jalr_ex), .pc_sel(pc_sel_ex), .pc_next(pc_next), .dm_addr(alu_ans_ex), .dm_din(32'h0),/*FIXME:*/ .dm_dout(32'h0), .dm_we(dm_we_ex), .check_addr(cpu_check_addr[4:0]), .check_data(check_data_ex));
     Check_Data_SEL check_data_sel_mem(.pc_cur(pc_cur_mem), .instruction(inst_mem), .rf_ra0(rf_ra0_mem), .rf_ra1(rf_ra1_mem), .rf_re0(rf_re0_mem), .rf_re1(rf_re1_mem), .rf_rd0_raw(rf_rd0_raw_mem), .rf_rd1_raw(rf_rd1_raw_mem), .rf_rd0(rf_rd0_mem), .rf_rd1(rf_rd1_mem), .rf_wa(rf_wa_mem), .rf_wd_sel(rf_wd_sel_mem), .rf_wd(32'h0), .rf_we(rf_we_mem), .immediate(imm_mem), .alu_src1(alu_src1_mem), .alu_src2(alu_src2_mem), .alu_func(alu_func_mem), .alu_ans(alu_ans_mem), .pc_add4(pc_add4_mem), .pc_br(pc_br_mem), .pc_jal(pc_jal_mem), .pc_jalr(pc_jalr_mem), .pc_sel(pc_sel_mem), .pc_next(pc_next_mem), .dm_addr(dm_addr_mem), .dm_din(dm_din_mem), .dm_dout(dm_dout), .dm_we(dm_we_mem), .check_addr(cpu_check_addr[4:0]), .check_data(check_data_mem));
     Check_Data_SEL check_data_sel_wb(.pc_cur(pc_cur_wb), .instruction(inst_wb), .rf_ra0(rf_ra0_wb), .rf_ra1(rf_ra1_wb), .rf_re0(rf_re0_wb), .rf_re1(rf_re1_wb), .rf_rd0_raw(rf_rd0_raw_wb), .rf_rd1_raw(rf_rd1_raw_wb), .rf_rd0(rf_rd0_wb), .rf_rd1(rf_rd1_wb), .rf_wa(rf_wa_wb), .rf_wd_sel(rf_wd_sel_wb), .rf_wd(rf_wd_wb), .rf_we(rf_we_wb), .immediate(imm_wb), .alu_src1(alu_src1_wb), .alu_src2(alu_src2_wb), .alu_func(alu_func_wb), .alu_ans(alu_ans_wb), .pc_add4(pc_add4_wb), .pc_br(pc_br_wb), .pc_jal(pc_jal_wb), .pc_jalr(pc_jalr_wb), .pc_sel(pc_sel_wb), .pc_next(pc_next_wb), .dm_addr(dm_addr_wb), .dm_din(dm_din_wb), .dm_dout(dm_dout_wb), .dm_we(dm_we_wb), .check_addr(cpu_check_addr[4:0]), .check_data(check_data_wb));
     Check_Data_SEL_HZD check_data_sel_hzd(.rf_ra0_ex(rf_ra0_ex), .rf_ra1_ex(rf_ra1_ex), .rf_re0_ex(rf_re0_ex), .rf_re1_ex(rf_re1_ex), .pc_sel_ex(pc_sel_ex), .rf_wa_mem(rf_wa_mem), .rf_we_mem(rf_we_mem), .rf_wd_sel_mem(rf_wd_sel_mem), .alu_ans_mem(alu_ans_mem), .pc_add4_mem(pc_add4_mem), .imm_mem(imm_mem), .rf_wa_wb(rf_wa_wb), .rf_we_wb(rf_we_wb), .rf_wd_wb(rf_wd_wb), .rf_rd0_fe(rf_rd0_fe), .rf_rd1_fe(rf_rd1_fe), .rf_rd0_fd(rf_rd0_fd), .rf_rd1_fd(rf_rd1_fd), .stall_if(stall_if), .stall_id(stall_id), .stall_ex(stall_ex), .flush_if(flush_if), .flush_id(flush_id), .flush_ex(flush_ex), .flush_mem(flush_mem), .check_addr(cpu_check_addr[4:0]), .check_data(check_data_hzd));
     Check_Data_SEG_SEL check_data_seg_sel(.check_data_if(check_data_if), .check_data_id(check_data_id), .check_data_ex(check_data_ex), .check_data_mem(check_data_mem), .check_data_wb(check_data_wb), .check_data_hzd(check_data_hzd), .check_addr(cpu_check_addr[7:5]), .check_data(check_data));
-    MUX1 cpu_check_data_sel(.sel(cpu_check_addr[12]), .src0(check_data), .src1(rd_dbg_id),/*FIXME:*/ .res(cpu_check_data));
+    MUX1 cpu_check_data_sel(.sel(cpu_check_addr[12]), .src0(check_data), .src1(rf_rd_dbg_id), .res(cpu_check_data));
 endmodule
