79f057a843c685ab0bdb7039641854185b748008
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2023-06-06 20:43:14 +0800

    Can barely work now

--- lab6/CPU_src/Exception.v
+++ lab6/CPU_src/Exception.v
@@ -4,16 +4,12 @@ module Exception (
     input [31:0] pc_next_ne,
     input [31:0] current_pc,
     output [31:0] pc_next,
-    output reg csr_we,
+    output csr_we,
     output [11:0] csr_wa,
     output [31:0] csr_wd
 );
     assign csr_wa = 12'h341;
     assign csr_wd = current_pc;
-    always @(posedge clk) begin
-        if (alu_overflow) begin
-            csr_we <= 1;
-        end else csr_we <= 0;
-    end
+    assign csr_we = alu_overflow;
     assign pc_next = alu_overflow ? 32'h4000 : pc_next_ne;
 endmodule
